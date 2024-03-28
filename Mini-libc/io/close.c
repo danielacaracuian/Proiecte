36c2dac7bc49b2f208db98f35f289aadfda0f523
danielacaracuian <118904945+danielacaracuian@users.noreply.github.com>
2024-03-28 13:40:48 +0200

    Create close.c

---
+++ Mini-libc/io/close.c
@@ -0,0 +1,20 @@
+// SPDX-License-Identifier: BSD-3-Clause
+
+#include <unistd.h>
+#include <internal/syscall.h>
+#include <stdarg.h>
+#include <errno.h>
+
+int close(int fd)
+{
+    /* TODO: Implement close(). */
+    int result = syscall(__NR_close, fd);
+
+    if (result == -1)
+    {
+        errno = EBADF;
+        return -1;
+    }
+
+    return 0;
+}
